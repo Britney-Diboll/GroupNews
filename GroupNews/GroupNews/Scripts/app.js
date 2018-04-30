98afe132ff7b589f1e4b6f836295772c10db714a
britney.diboll <britney.diboll@gmail.com>
2018-04-30 18:08:16 -0400

    Getting up to date

---
+++ GroupNews/GroupNews/Scripts/app.js
@@ -0,0 +1,49 @@
+﻿const app = angular.module("main", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/news/stories", {
+        templateUrl: "/Scripts/app/views/Dashboard.html",
+        controller: "mainNewsController"
+    })
+
+    $routeProvider.when("/news/stories/:storyID", {
+        templateUrl: "/Scripts/app/views/StoryPage.html",
+        controller: "storyPageController"
+    })
+
+    $routeProvider.when("/news/create-story", {
+        templateUrl: "/Scripts/app/views/CreateStory.html",
+        controller: "createStoryController"
+    })
+
+    $routeProvider.otherwise({ redirectTo: "/news/stories" });
+})
+
+app.controller("mainNewsController", ["$scope", "$http", function ($scope, $http) {
+
+    //const getEvents = () => {
+    //    let searchURL = "/api/news";
+    //    if ($scope.searchQuery) {
+    //        searchURL = searchURL + "?title=" + $scope.searchQuery
+    //    }
+    //    $http({
+    //        method: "GET",
+    //        url: searchURL
+    //    }).then(resp => {
+    //        console.log(resp.data);
+    //        $scope.events = resp.data;
+    //    })
+    //}
+
+    //$scope.searchForEvent = () => {
+    //    getEvents();
+    //}
+
+    var start = () => {
+        $scope.pageTitle = "testing";
+        //getEvents();
+    }
+
+    start();
+}])
